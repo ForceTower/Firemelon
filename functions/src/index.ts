e3b8128feddd3ffaaf73d905fbac4b6a315290ca
João Paulo Sena <joaopaulo761@gmail.com>
2018-12-04 00:43:03 -0300

    Help users migrate from unes to unesx

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,8 @@ const database = admin.firestore();
 const settings = { timestampsInSnapshots: true };
 database.settings(settings);
 
+const storage = admin.storage().bucket()
+
 export const eventsUpdate = functions.firestore
     .document("events/{eventId}")
     .onUpdate(async(snapshot) => {
@@ -29,7 +31,7 @@ export const eventsUpdate = functions.firestore
         }
     });
 
-export const adminMessages = functions.firestore.document("unes_messages/{messageId}")
+export const adminMessages = functions.firestore.document("unes_notify_messages/{messageId}")
     .onCreate(async(snapshot) => {
         const payload = {
             data: {
@@ -39,8 +41,65 @@ export const adminMessages = functions.firestore.document("unes_messages/{messag
                 image: snapshot.data()['image'] || null
             }
         }
+
+        const data = snapshot.data()
+        await database.collection("unes_messages").add({
+            title: data['title'],
+            message: data['data'],
+            image: data['image'] || null,
+            link: data['link'] || 'https://github.com/ForceTower/Melon',
+            createdAt: snapshot.readTime
+        })
         await notifyUsers(payload);
         return true;
+    });
+
+export const migrateAccount = functions.firestore.document("users/{userId}")
+    .onCreate(async(snapshot) => {
+        const data = snapshot.data()
+        const username = data['username']
+        const currId = snapshot.id
+        if (username != null) {
+            const usersQuery = await database.collection('users')
+                .where('username', '==', username)
+                .get()
+
+            const users = usersQuery.docs
+                .filter(user => user.id != currId)
+
+            if (users != null && users.length > 0) {
+                const prevId = users[0].id
+
+                const reminders = await database.collection('users').doc(prevId).collection('reminders').get()
+                reminders.docs.map(async(reminder) => {
+                    const plain = reminder.data()
+                    await database.collection('users').doc(currId).collection('reminders').add(plain)
+                });
+
+                const fileExists = await storage.file('users/' + prevId + '/avatar.jpg').exists()
+                if (fileExists) {
+                    try {
+                    await storage.file('users/' + prevId + '/avatar.jpg').move('users/' + currId + '/avatar.jpg')
+                    console.log('<< Old picture copied')
+                    } catch (e) {
+                        console.log('<< Guck exception')
+                    }
+                }
+                
+                await database.collection('users').doc(prevId).delete()
+                try {
+                    await admin.auth().deleteUser(prevId)
+                } catch (e) {
+                    console.log('<< Guck exception for delete account')
+                }
+
+                console.log('>> Completed migration for user ' + username)
+            } else {
+                console.log('>> Previous user did not match')
+            }
+        } else {
+            console.log('>> Previous user was undefined')
+        }
     });
 
 async function notifyUsers(payload: admin.messaging.MessagingPayload) {
@@ -48,8 +107,7 @@ async function notifyUsers(payload: admin.messaging.MessagingPayload) {
     const tokens: string[] = documents.docs
         .map(val => val.data())
         .map(val => val['firebaseToken'])
-        .filter(val => val != null)
-        .filter(val => val.length > 0)
+        .filter(val => val != null && val.length > 0)
     
     console.log("> Size: " + tokens.length)
 
